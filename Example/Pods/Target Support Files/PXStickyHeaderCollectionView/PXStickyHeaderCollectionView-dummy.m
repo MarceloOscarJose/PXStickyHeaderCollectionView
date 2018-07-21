a78bd7d4593af771df103eabcb562546075d6821
Marcelo José <marcelo.jose@mercadolibre.com>
2018-07-21 02:27:37 -0300

    Delete files

--- Example/Pods/Target Support Files/PXStickyHeaderCollectionView/PXStickyHeaderCollectionView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PXStickyHeaderCollectionView : NSObject
-@end
-@implementation PodsDummy_PXStickyHeaderCollectionView
-@end
--- Example/Pods/Target Support Files/Pods-PXStickyHeaderCollectionView_Example/Pods-PXStickyHeaderCollectionView_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PXStickyHeaderCollectionView_Example : NSObject
-@end
-@implementation PodsDummy_Pods_PXStickyHeaderCollectionView_Example
-@end
